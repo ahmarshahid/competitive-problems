28c881275c8e48acb9ad22bd55161b0c0d239ebe
Muhammad Ahmar Shahid <120157630+ahmarshahid@users.noreply.github.com>
2026-08-12 00:43:34 +0500

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        detectZero = []
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i]==0:
+                detectZero.append(nums[i])
+                del nums[i] 
+        if len(detectZero) != 0:
+            for j in detectZero:
+                nums.append(detectZero[j])
+        return nums
+        
